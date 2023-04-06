bcf232dbfa5ece108751285484829545f814a56f
Егор Олейник <egorolejnik@MacBook-Air-Vadim.local>
2023-04-06 19:51:23 +0300

    First initial

---
+++ CapsShop_AspPractise/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CapsShop_AspPractise.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
